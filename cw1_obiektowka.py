0bf83559b8cafa0a57384e232635f0c1e8fdf596
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-16 13:01:39 +0100

    update

--- cw1_obiektowka.py
+++ cw1_obiektowka.py
@@ -1,6 +1,9 @@
 from random import randint
 
 class Drone():
+    """
+    Klasa służąca do obsługi dronów. Przymuje wartość 'speed' jak prędkość.
+    """
     def __init__(self, speed):
         self.altitude = 0
         self.speed = speed
@@ -9,12 +12,25 @@ class Drone():
         return "Dron o mocy:" + str(self.speed) + ", na wysokości: " + str(self.altitude)
 
     def moveUp(self):
+        """
+        Ta metoda porusza dronem w górę o prędkość, jaką ma dany dron
+        :return:
+        """
         self.altitude += self.speed
 
+    def moveDown(self):
+        # if (self.altitude - self.speed) < 0:
+        #     self.altitude = 0
+        # else:
+        #     self.altitude -= self.speed
+        self.altitude = max(0, self.altitude - self.speed)
+
+    def changeSpeed(self, new_speed):
+        self.speed = new_speed
 
 # lista do przechowania 10 dronów
 drones = list()
-
+#
 # wykorzystana petla do stworzenia 10 elementów
 # for _ in range(10):
 #     # tworze nowego drona wykorzystujac forme Drone()
@@ -30,6 +46,17 @@ for i in range(1000):
     droneIndex = randint(0, 9)
     drones[droneIndex].moveUp()
 
+# obniżenie losowego drona w góre
+for i in range(1000):
+    droneIndex = randint(0, 9)
+    drones[droneIndex].moveDown()
+
+
 # wyświetlnie dronów
 for drone in drones:
     print(drone)
+
+
+print("Przed zmianą:", drones[0])
+drones[0].changeSpeed(20)
+print("Po zmianie:", drones[0])
